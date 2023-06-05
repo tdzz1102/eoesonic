92d882eb2976e00252b55308db358079df0e03b5
tdzz1102 <wasuremono127@gmail.com>
2023-06-05 22:10:01 +0800

    create user by st

--- scripts/index.py
+++ scripts/index.py
@@ -1,24 +1,28 @@
 import streamlit as st
 import requests
+import random
+import string
+
+
+def generate_random_string(length):
+    characters = string.ascii_letters + string.digits
+    random_string = ''.join(random.choices(characters, k=length))
+    return random_string
 
 
 def create_user(username, password):
-    # ログイン用のURLとデータ
     login_url = "https://app.eoesonic.com/auth/login"
     login_data = {
         "username": 'admin_user',
         "password": 'admin_pass'
     }
 
-    # ログインリクエストを送信
     login_response = requests.post(login_url, json=login_data, verify=False)
     login_response_json = login_response.json()
 
-    # トークンを取得
     token = login_response_json.get("token")
 
     if token:
-        # ユーザー作成用のURLとデータ
         create_user_url = "https://app.eoesonic.com/api/user"
         headers = {
             "x-nd-authorization": f"Bearer {token}"
@@ -26,34 +30,31 @@ def create_user(username, password):
         user_data = {
             "isAdmin": False,
             "userName": username,
-            "name": "Random8",  # 任意の8文字
+            "name": generate_random_string(12),  # 任意の12文字
             "password": password
         }
-
-        # ユーザー作成リクエストを送信
         create_user_response = requests.post(create_user_url, headers=headers, json=user_data, verify=False)
         create_user_response_json = create_user_response.json()
 
-        # レスポンスを返す
         return create_user_response_json
-
-    # トークンが取得できなかった場合はNoneを返す
     return None
 
 
-st.title("create user")
+st.title("EOESonic 用户注册")
 
 
 with st.form("my_form"):
     username = st.text_input('username')
     password = st.text_input('password', type='password')
     
-    st.write("Inside the form")
-    submitted = st.form_submit_button("Submit")
+    submitted = st.form_submit_button("确认")
     if submitted:
         create_user_response_json = create_user(username, password)
+        if "id" in create_user_response_json:
+            st.success("OK.")
+        else:
+            st.error("Not OK.")
         st.json(create_user_response_json)
 
 
-st.write("Outside the form")
-st.markdown('[homepage](https://eoesonic.com)')
+st.markdown('[主页](https://eoesonic.com)')
